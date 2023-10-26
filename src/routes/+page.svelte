57d8600d1706d7edc8f926c92a152d5b61e80e2f
rinjugatla <rin.jugatla@gmail.com>
2023-10-27 03:37:52 +0900

    フォームデザインをflowbiteに変更

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { Input, Label, Button } from 'flowbite-svelte';
 	import type { TwitchClipResponse, TwitchUserResponse } from "$lib/server/twitch";
 	import { json } from "@sveltejs/kit";
 	import axios from 'redaxios';
@@ -9,18 +10,25 @@
 </script>
 
 <form>
-    <input type="text" bind:value={username}>
-    <button type="button" on:click={async () => {
-        const users = await axios.post(
-            '/api/twitch/users',
-            { name: username }
-        );
-        userData = users.data.data[0];
-
-        const clips = await axios.post(
-            '/api/twitch/clips',
-            {id: userData.id}
-        )
-        clipData = clips.data;
-    }} >取得</button>
+    <div class="grid gap-6 mb-6 md:grid-cols-2">
+        <div>
+          <Label for="name" class="mb-2">Twitchユーザ名</Label>
+          <Input type="text" id="name" bind:value={username} required />
+        </div>
+    </div>
+    <div  class="grid gap-6 mb-6 md:grid-cols-2">
+        <Button on:click={async () => {
+            const users = await axios.post(
+                '/api/twitch/users',
+                { name: username }
+            );
+            userData = users.data.data[0];
+    
+            const clips = await axios.post(
+                '/api/twitch/clips',
+                {id: userData.id}
+            )
+            clipData = clips.data;
+        }}>取得</Button>
+    </div>
 </form>
