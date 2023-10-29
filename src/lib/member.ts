9863c9961fef015dfe0e60236fd79982dd33ed48
rinjugatla <rin.jugatla@gmail.com>
2023-10-29 18:06:03 +0900

    主催、HibiscusCupの配信IDを追加

--- src/lib/member.ts
+++ src/lib/member.ts
@@ -1,5 +1,6 @@
 // 企画者
 export const HIBISCUS_CUP_SPONSER = [
+    { role: '主催', name: 'りえぺこ', twitch: 'riepeko', twitter: 'Riepeko_yolo', video_id: ['1956481664'] },
 	{ role: '企画', name: 'だいん', twitch: null, twitter: 'DAiN87006586' },
 	{ role: '運営', name: 'Aihara games', twitch: 'aiharagames', twitter: 'AiharaGames' },
 	{ role: '運営', name: 'kshrrr', twitch: null, twitter: 'ksnon_' },
@@ -11,39 +12,39 @@ export const HIBISCUS_CUP_SPONSER = [
 // 参加者
 export const HIBISCUS_CUP_TEAMS = {
 	A: [
-		{ name: '蒼音カノン', twtich: 'aone_kanon', twitter: 'aone_kanon' },
-		{ name: 'あぷべり', twtich: 'appleberry_xx', twitter: 'snoopy_gamble' }
+		{ name: '蒼音カノン', twtich: 'aone_kanon', twitter: 'aone_kanon', video_id: ['1956475542', '1956530908'] },
+		{ name: 'あぷべり', twtich: 'appleberry_xx', twitter: 'snoopy_gamble', video_id: ['', ] }
 	],
 	B: [
-		{ name: 'るぴぃ', twtich: 'iamrupi', twitter: 'iamrupii' },
-		{ name: '相生あい', twtich: 'aioi_ai', twitter: 'AioiAi_v' }
+		{ name: 'るぴぃ', twtich: 'iamrupi', twitter: 'iamrupii', video_id: ['1956470883', ] },
+		{ name: '相生あい', twtich: 'aioi_ai', twitter: 'AioiAi_v', video_id: ['1956461868', ] }
 	],
 	C: [
-		{ name: 'れおーな', twtich: 'reona_vt', twitter: 'Reo_NaGG' },
-		{ name: '桃井メイ', twtich: 'momoimei', twitter: 'momoimei' }
+		{ name: 'れおーな', twtich: 'reona_vt', twitter: 'Reo_NaGG', video_id: ['1956470781', ] },
+		{ name: '桃井メイ', twtich: 'momoimei', twitter: 'momoimei', video_id: ['1956424935', ] }
 	],
 	D: [
-		{ name: 'あんぱん_', twtich: 'ampm_m', twitter: '_ampm_mm' },
-		{ name: '佐藤らむね', twtich: '310ramune', twitter: '310ramune' }
+		{ name: 'あんぱん_', twtich: 'ampm_m', twitter: '_ampm_mm', video_id: ['1956479528', ] },
+		{ name: '佐藤らむね', twtich: '310ramune', twitter: '310ramune', video_id: ['1956479572', ] }
 	],
 	E: [
-		{ name: 'べるるる', twtich: 'bell0717', twitter: 'bell0717_' },
-		{ name: '辰星マキリ', twtich: 'tachimiboshimakiri', twitter: 'makiriRPG' }
+		{ name: 'べるるる', twtich: 'bell0717', twitter: 'bell0717_', video_id: ['1956481869', ] },
+		{ name: '辰星マキリ', twtich: 'tachimiboshimakiri', twitter: 'makiriRPG', video_id: ['1956481159', ] }
 	],
 	F: [
-		{ name: '御握利もぐ', twtich: 'mogu_vtuber', twitter: 'Onigiri_MoguV' },
-		{ name: 'おひつじめぇ', twtich: 'ohitsu_zi', twitter: 'ohi_mee' }
+		{ name: '御握利もぐ', twtich: 'mogu_vtuber', twitter: 'Onigiri_MoguV', video_id: ['1956482411', ] },
+		{ name: 'おひつじめぇ', twtich: 'ohitsu_zi', twitter: 'ohi_mee', video_id: ['1956482731', '1957305365'] }
 	],
 	G: [
-		{ name: 'ギルテックス', twtich: 'gilltex', twitter: 'GillTeX_Twitch' },
-		{ name: 'がおにゃ', twtich: 'gaonya', twitter: 'pashirineko' }
+		{ name: 'ギルテックス', twtich: 'gilltex', twitter: 'GillTeX_Twitch', video_id: ['1956471945', ] },
+		{ name: 'がおにゃ', twtich: 'gaonya', twitter: 'pashirineko', video_id: ['1956471973', ] }
 	],
 	H: [
-		{ name: 'えるとね', twtich: 'ltonewm', twitter: 'ltonewmV2' },
-		{ name: 'べこぉ', twtich: 'be_kooo', twitter: 'ch_beko' }
+		{ name: 'えるとね', twtich: 'ltonewm', twitter: 'ltonewmV2', video_id: ['1956474786', ] },
+		{ name: 'べこぉ', twtich: 'be_kooo', twitter: 'ch_beko', video_id: ['1956474886', ] }
 	],
 	I: [
-		{ name: 'higu_zz', twtich: 'higu_zz', twitter: 'higu_zZ_TTV' },
-		{ name: 'DEIcecreeeam', twtich: 'deicecreeeam', twitter: 'deicecreeeam' }
+		{ name: 'higu_zz', twtich: 'higu_zz', twitter: 'higu_zZ_TTV', video_id: ['1956479343', '1959043150'] },
+		{ name: 'DEIcecreeeam', twtich: 'deicecreeeam', twitter: 'deicecreeeam', video_id: ['1956479286', ] }
 	]
 };
