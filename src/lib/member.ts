f09d2c55f28badf26483769b9c1490f46f45bd1f
rinjugatla <rin.jugatla@gmail.com>
2023-10-30 01:17:52 +0900

    主催者、参加者一覧を列挙する機能を追加
    デザイン要修正

--- src/lib/member.ts
+++ src/lib/member.ts
@@ -10,41 +10,23 @@ export const HIBISCUS_CUP_SPONSER = [
 ];
 
 // 参加者
-export const HIBISCUS_CUP_TEAMS = {
-	A: [
-		{ name: '蒼音カノン', twtich: 'aone_kanon', twitter: 'aone_kanon', video_id: ['1956475542', '1956530908'] },
-		{ name: 'あぷべり', twtich: 'appleberry_xx', twitter: 'snoopy_gamble', video_id: [] }
-	],
-	B: [
-		{ name: 'るぴぃ', twtich: 'iamrupi', twitter: 'iamrupii', video_id: ['1956470883', ] },
-		{ name: '相生あい', twtich: 'aioi_ai', twitter: 'AioiAi_v', video_id: ['1956461868', ] }
-	],
-	C: [
-		{ name: 'れおーな', twtich: 'reona_vt', twitter: 'Reo_NaGG', video_id: ['1956470781', ] },
-		{ name: '桃井メイ', twtich: 'momoimei', twitter: 'momoimei', video_id: ['1956424935', ] }
-	],
-	D: [
-		{ name: 'あんぱん_', twtich: 'ampm_m', twitter: '_ampm_mm', video_id: ['1956479528', ] },
-		{ name: '佐藤らむね', twtich: '310ramune', twitter: '310ramune', video_id: ['1956479572', ] }
-	],
-	E: [
-		{ name: 'べるるる', twtich: 'bell0717', twitter: 'bell0717_', video_id: ['1956481869', ] },
-		{ name: '辰星マキリ', twtich: 'tachimiboshimakiri', twitter: 'makiriRPG', video_id: ['1956481159', ] }
-	],
-	F: [
-		{ name: '御握利もぐ', twtich: 'mogu_vtuber', twitter: 'Onigiri_MoguV', video_id: ['1956482411', ] },
-		{ name: 'おひつじめぇ', twtich: 'ohitsu_zi', twitter: 'ohi_mee', video_id: ['1956482731', '1957305365'] }
-	],
-	G: [
-		{ name: 'ギルテックス', twtich: 'gilltex', twitter: 'GillTeX_Twitch', video_id: ['1956471945', ] },
-		{ name: 'がおにゃ', twtich: 'gaonya', twitter: 'pashirineko', video_id: ['1956471973', ] }
-	],
-	H: [
-		{ name: 'えるとね', twtich: 'ltonewm', twitter: 'ltonewmV2', video_id: ['1956474786', ] },
-		{ name: 'べこぉ', twtich: 'be_kooo', twitter: 'ch_beko', video_id: ['1956474886', ] }
-	],
-	I: [
-		{ name: 'higu_zz', twtich: 'higu_zz', twitter: 'higu_zZ_TTV', video_id: ['1956479343', '1959043150'] },
-		{ name: 'DEIcecreeeam', twtich: 'deicecreeeam', twitter: 'deicecreeeam', video_id: ['1956479286', ] }
-	]
-};
+export const HIBISCUS_CUP_MEMBER = [
+	{ team: 'A', name: '蒼音カノン', twtich: 'aone_kanon', twitter: 'aone_kanon', video_id: ['1956475542', '1956530908'] },
+	{ team: 'A', name: 'あぷべり', twtich: 'appleberry_xx', twitter: 'snoopy_gamble', video_id: [] },
+	{ team: 'B', name: 'るぴぃ', twtich: 'iamrupi', twitter: 'iamrupii', video_id: ['1956470883', ] },
+	{ team: 'B', name: '相生あい', twtich: 'aioi_ai', twitter: 'AioiAi_v', video_id: ['1956461868', ] },
+	{ team: 'C', name: 'れおーな', twtich: 'reona_vt', twitter: 'Reo_NaGG', video_id: ['1956470781', ] },
+	{ team: 'C', name: '桃井メイ', twtich: 'momoimei', twitter: 'momoimei', video_id: ['1956424935', ] },
+	{ team: 'D', name: 'あんぱん_', twtich: 'ampm_m', twitter: '_ampm_mm', video_id: ['1956479528', ] },
+	{ team: 'D', name: '佐藤らむね', twtich: '310ramune', twitter: '310ramune', video_id: ['1956479572', ] },
+	{ team: 'E', name: 'べるるる', twtich: 'bell0717', twitter: 'bell0717_', video_id: ['1956481869', ] },
+	{ team: 'E', name: '辰星マキリ', twtich: 'tachimiboshimakiri', twitter: 'makiriRPG', video_id: ['1956481159', ] },
+	{ team: 'F', name: '御握利もぐ', twtich: 'mogu_vtuber', twitter: 'Onigiri_MoguV', video_id: ['1956482411', ] },
+	{ team: 'F', name: 'おひつじめぇ', twtich: 'ohitsu_zi', twitter: 'ohi_mee', video_id: ['1956482731', '1957305365'] },
+	{ team: 'G', name: 'ギルテックス', twtich: 'gilltex', twitter: 'GillTeX_Twitch', video_id: ['1956471945', ] },
+	{ team: 'G', name: 'がおにゃ', twtich: 'gaonya', twitter: 'pashirineko', video_id: ['1956471973', ] },
+	{ team: 'H', name: 'えるとね', twtich: 'ltonewm', twitter: 'ltonewmV2', video_id: ['1956474786', ] },
+	{ team: 'H', name: 'べこぉ', twtich: 'be_kooo', twitter: 'ch_beko', video_id: ['1956474886', ] },
+	{ team: 'I', name: 'higu_zz', twtich: 'higu_zz', twitter: 'higu_zZ_TTV', video_id: ['1956479343', '1959043150'] },
+	{ team: 'I', name: 'DEIcecreeeam', twtich: 'deicecreeeam', twitter: 'deicecreeeam', video_id: ['1956479286', ] },
+];
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,144 @@
 <script lang="ts">
-    import { Input, Label, Button } from 'flowbite-svelte';
-	import { json } from "@sveltejs/kit";
 	import axios from 'redaxios';
-	import Clips from '$lib/components/twtich/Clips.svelte';
-	import type { TwitchClip, TwitchClipResponse, TwitchUser, TwitchUserResponse } from '$lib/server/twitch';
+	import type { TwitchUser, TwitchUserResponse } from '$lib/server/twitch';
+	import { HIBISCUS_CUP_MEMBER, HIBISCUS_CUP_SPONSER } from '$lib/member';
+	import { onMount } from 'svelte';
 
-    let username: string;
-    let userData: TwitchUser;
-    let clipData: TwitchClipResponse;
+    let sponserProfiles: HibiscusCupSponser[];
+    let teamMemberProfiles: HibiscusCupTeamMember[];
 
-    const getClips = async () => {
-        const users = await axios.post<TwitchUserResponse>(
-                '/api/twitch/users',
-                { names: [username] }
-            );
-            userData = users.data.users[0];
-    
-            const clips = await axios.post<TwitchClipResponse>(
-                '/api/twitch/clips',
-                {id: userData.id}
-            )
-            clipData = clips.data;
+    class HibiscusCupBase {
+        private _Name: string;
+        private _Twitch: TwitchUser | undefined;
+        private _YoutubeId: string;
+
+        constructor (name: string, twitch: TwitchUser | undefined, youtubeId: string) {
+            this._Name = name;
+            this._Twitch = twitch;
+            this._YoutubeId = youtubeId;
+        }
+
+        get Name (): string {
+            return this._Name;
+        }
+
+        get TwitchProfileImageLink (): string | undefined {
+            return this._Twitch?.profile_image_url;
+        }
+
+        // 自己紹介
+        get Description (): string | undefined {
+            return this._Twitch?.description
+        }
+
+        get YoutubeLink (): string {
+            const link = `https://www.youtube.com/${this._YoutubeId}`;
+            return link;
+        }
     }
+
+    class HibiscusCupSponser extends HibiscusCupBase {
+        private _Role: string;
+
+        constructor (name: string, twitch: TwitchUser | undefined, youtubeId: string, role: string){
+            super(name, twitch, youtubeId);
+            this._Role = role;
+        }
+
+        get Role(): string {
+            return this._Role;
+        }
+    }
+
+    class HibiscusCupTeamMember extends HibiscusCupBase {
+        private _Team: string;
+
+        constructor (name: string, twitch: TwitchUser, youtubeId: string, team: string){
+            super(name, twitch, youtubeId);
+            this._Team = team;
+        }
+
+        get Team(): string {
+            return this._Team;
+        }
+    }
+
+    const getTeamMemberProfiles = async (): Promise<HibiscusCupTeamMember[]> => {
+        const userNames = HIBISCUS_CUP_MEMBER.map((member) => member.twtich);
+        const twitchUsers = await getTwitchUsers(userNames);
+
+        let teamProfiles: HibiscusCupTeamMember[] = [];
+        for (const user of twitchUsers) {
+            const info = HIBISCUS_CUP_MEMBER.find((member) => member.twtich === user.login)!;
+            const memberProfile = new HibiscusCupTeamMember(info.name, user, '', info.team);
+            teamProfiles.push(memberProfile);
+        }
+
+        return teamProfiles;
+    }
+
+    const getSponserProfiles = async (): Promise<HibiscusCupSponser[]> => {
+        const userNames = HIBISCUS_CUP_SPONSER.filter((member) => member.twitch != null).
+                                               map((member) => member.twitch!);
+        const twitchUsers = await getTwitchUsers(userNames);
+
+        let sponserProfiles: HibiscusCupSponser[] = [];
+        // twitch情報がないユーザもいるのでHIBISCUS_CUP_SPONSER基準
+        for (const sponser of HIBISCUS_CUP_SPONSER) {
+            const user = twitchUsers.find((user) => user.login === sponser.twitch);
+            const sponserProfile = new HibiscusCupSponser(sponser.name, user, '', sponser.role);
+            sponserProfiles.push(sponserProfile);
+        }
+
+        return sponserProfiles;
+    }
+
+    const getTwitchUsers = async (userNames: string[]): Promise<TwitchUser[]> => {
+        const response = await axios.post<TwitchUserResponse>(
+            '/api/twitch/users',
+            { names: userNames }
+        );
+
+        const users = response.data.users;
+        return users;
+    }
+
+    onMount(async () => {
+        sponserProfiles = await getSponserProfiles();
+        console.log(sponserProfiles);
+
+        teamMemberProfiles = await getTeamMemberProfiles();
+        console.log(teamMemberProfiles);
+    });
 </script>
 
-<form>
-    <div class="grid gap-6 mb-6">
-        <div>
-          <Label for="name" class="mb-2">Twitchユーザ名</Label>
-          <Input type="text" id="name" bind:value={username} required />
-        </div>
-    </div>
-    <div  class="grid gap-6 mb-6">
-        <Button on:click={async () => {getClips();}}>取得</Button>
-    </div>
-</form>
 
-{#if clipData != null}
-    <Clips clips={clipData.clips}></Clips>     
-{/if}
+
+<div>
+    {#if !sponserProfiles}
+        <p>loading...</p>
+    {:else}
+        {#each sponserProfiles as sponser}
+            <div>
+                <p>{sponser.Role}</p>
+                <p>{sponser.Name}</p>
+            </div>
+        {/each}
+    {/if}
+
+</div>
+
+<div>
+
+
+    {#if !teamMemberProfiles}
+    <p>loading...</p>
+    {:else}
+        {#each teamMemberProfiles as member}
+            <div>
+                <p>{member.Team}</p>
+                <p>{member.Name}</p>
+            </div>
+        {/each}
+    {/if}
+</div>
