515967c7a74b5ecb262c838be56c7f5f09594ffc
rinjugatla <rin.jugatla@gmail.com>
2023-11-03 03:10:42 +0900

    Streamsページを開いた際に関連ビデオ情報を取得する機能を追加

--- src/routes/streams/+page.svelte
+++ src/routes/streams/+page.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
 	import { HIBISCUS_CUP_SPONSER, HIBISCUS_CUP_MEMBER } from '$lib/member';
 	import type { HTMLImgAttributes } from 'svelte/elements';
+    import axios from 'redaxios';
 
 	import Carousel from '$lib/components/flowbite-svelte-costoms/carousel/Carousel.svelte';
 	import Thumbnails from '$lib/components/flowbite-svelte-costoms/carousel/Thumbnails.svelte';
+	import type { ITwitchVideoResponse } from '$lib/types';
+	import { onMount } from 'svelte';
 
 	let index = 0;
+    let selectedImage: HTMLImgAttributes;
 	let forward = true; // sync animation direction between Thumbnails and Carousel
+    let streamInfos: {
+        [key: string]: 
+        {
+            thumbnail_url: string, 
+            url: string
+        }} = {};
 
 	const profile_image_prefix = 'https://pbs.twimg.com/profile_images/';
 	const createProfileImageAttributes = (): HTMLImgAttributes[] => {
@@ -17,20 +27,49 @@
 
             attributes.push({
 				src: profile_image_prefix + sponser.profile_image,
-				alt: sponser.name
+				alt: sponser.twitch
 			});
         }
 
 		for (const member of HIBISCUS_CUP_MEMBER) {
 			attributes.push({
 				src: profile_image_prefix + member.profile_image,
-				alt: member.name
+				alt: member.twitch
 			});
 		}
 		return attributes;
 	};
 
 	const images = createProfileImageAttributes();
+
+    const getStreams = async () => {
+        const sponsersStreamIds = new Set(HIBISCUS_CUP_SPONSER.filter((s) => s.video_id.length > 0).map((s) => s.video_id).flat());
+        const memberStreamIds = new Set(HIBISCUS_CUP_MEMBER.filter((m) => m.video_id.length > 0).map((m) => m.video_id).flat())
+        const streamIds = [Array.from(sponsersStreamIds), Array.from(memberStreamIds)].flat();
+
+        const response = await axios.post<ITwitchVideoResponse>(
+            '/api/twitch/videos',
+            {ids: streamIds}
+        );
+
+        const streams = response.data.videos;
+        return streams;
+    };
+
+    const setStreamInfo = async () => {
+        const streams = await getStreams();
+
+        for (const stream of streams) {
+            streamInfos[stream.user_login] = {
+                thumbnail_url: stream.thumbnail_url,
+                url: stream.url
+            }
+        }
+    };
+    
+    onMount(async () => {
+        await setStreamInfo();
+    })
 </script>
 
 <div class="flex flex-col items-center">
@@ -38,6 +77,6 @@
         <Carousel {images} {forward} transition={null} slideDuration={500} let:Controls bind:index>
             <Controls />
         </Carousel>
-        <Thumbnails {images} {forward} throttleDelay={250} imgClass="h-[50px] w-auto" bind:index />
+        <Thumbnails {images} {forward} throttleDelay={250} imgClass="h-[50px] w-auto" bind:index bind:selectedImage />
     </div>
 </div>
