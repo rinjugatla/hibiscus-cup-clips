d16293da8b82a76f8eb64ddea4b4f16a89376717
rinjugatla <rin.jugatla@gmail.com>
2023-11-04 03:27:15 +0900

    スポンサーリストをチームリストと同じデザインに修正
    デザインがいまいちなので直したい

--- src/lib/components/list/Sponser.svelte
+++ src/lib/components/list/Sponser.svelte
@@ -7,20 +7,22 @@
     const profile_image_prefix = 'https://pbs.twimg.com/profile_images/';
 
     export let sponsers: IHibiscusCupSponserInfo[];
+    let role = sponsers.map((sponser) => sponser.role)[0];
 </script>
 
-{#each sponsers as sponser}
-    <div class="m-2">
-        <Card padding="sm">
+
+<div class="m-2">
+    <Card padding="sm" horizontal={true}>
+        <span class="text-lg text-gray-500 dark:text-gray-400 mr-3">{role}</span>
+        {#each sponsers as sponser}
             <div class="flex flex-col items-center">
                 <Avatar size="lg" src="{profile_image_prefix}{sponser.profile_image}" />
                 <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{sponser.name}</h5>
-                <span class="text-sm text-gray-500 dark:text-gray-400">{sponser.role}</span>
                 <div class="flex mt-4 space-x-3 lg:mt-6">
                     <TwitchLinkIcon name={sponser.twitch} />
                     <TwitterLinkIcon name={sponser.twitter} />
                 </div>
             </div>
-        </Card>
-    </div>
-{/each}
+        {/each}
+    </Card>
+</div>
