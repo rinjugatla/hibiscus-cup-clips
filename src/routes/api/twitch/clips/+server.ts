bc6cd168b7ae7093230b89f006aca42e6bf1f396
rinjugatla <rin.jugatla@gmail.com>
2023-11-03 19:10:50 +0900

    中間APIでリクエスト元の判定方法を変更
    deno deployで自サイトからのリクエストかの判定に失敗している

--- src/routes/api/twitch/clips/+server.ts
+++ src/routes/api/twitch/clips/+server.ts
@@ -3,7 +3,7 @@ import { json } from "@sveltejs/kit";
 import { get } from "svelte/store";
 
 export async function POST({ request }): Promise<Response> {
-    const isSelfRequest = request.credentials == 'same-origin';
+    const isSelfRequest = request.headers.get('sec-fetch-site') === 'same-origin';
     if (!isSelfRequest){ return json(new Response()); }
     
     const api = get(twitchApi);
