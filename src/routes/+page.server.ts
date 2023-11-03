720701f8621f0cf67031369e7d806aff7054f4b7
rinjugatla <rin.jugatla@gmail.com>
2023-11-03 18:09:35 +0900

    Github Actionから環境変数を利用できるよう読み込み方をViteを利用する方法に変更

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,12 @@
-import { CLIENT_ID, CLIENT_SECRET } from '$env/static/private';
 import { TwitchApi, TwitchApiSetting } from '$lib/server/twitch';
 import { twitchApi } from '$lib/store';
 
 // TwitchAPI初期化
 twitchApi.subscribe((value) => {
 	if (!value) {
+		const CLIENT_ID = import.meta.env.VITE_CLIENT_ID as string;
+		const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET as string;
+
 		const twitchSetting = new TwitchApiSetting(CLIENT_ID, CLIENT_SECRET);
 		twitchApi.set(new TwitchApi(twitchSetting));
 	}
