db379437181f68be70ac6dc66e0f635fedbfde1b
rinjugatla <rin.jugatla@gmail.com>
2023-11-03 17:15:12 +0900

    フッターを追加

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
 	import '../app.postcss';
 	import Header from '$lib/components/common/Header.svelte';
+	import Footer from '$lib/components/common/Footer.svelte';
 </script>
 
 <Header />
 <main>
 	<slot />
 </main>
+<Footer />
