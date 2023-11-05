4ecf804e4819971860f11c20fea82731ae24c6f2
rinjugatla <rin.jugatla@gmail.com>
2023-11-05 22:42:02 +0900

    すべてのクリップを読み込むと重たいので最初はサムネイルを表示しておくよう変更

--- src/lib/components/twtich/Clip.svelte
+++ src/lib/components/twtich/Clip.svelte
@@ -9,13 +9,19 @@
 
 <div class="m-1 p-2.5 bg-white w-[660px] rounded-md">
 	<div class="flex flex-col">
-		<iframe
+		{#if !clicked}
+			<button type="button" on:click={() => clicked = true}>
+				<img src="{clip.thumbnail_url}" alt="{clip.title}" height="360" width="640">
+			</button>
+		{:else}
+			<iframe
 			src="https://clips.twitch.tv/embed?clip={clip.id}&parent={$page.url.hostname}"
 			allowfullscreen
 			preload="metadata"
 			height="360"
 			width="640"
 			title={clip.title}></iframe>
+		{/if}
 		<div class="px-5">
 			<h5 class="text-lg text-gray-500">{clip.creator_name}</h5>
 			<span class="text-3xl font-bold text-gray-900">{clip.title}</span>
